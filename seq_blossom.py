d13302a24cbc50d23703dc95d84260c2c4649b60
sagarvare <svare@stanford.edu>
2016-05-10 10:39:46 -0700

    Added Random Graph generator function

--- seq_blossom.py
+++ seq_blossom.py
@@ -14,6 +14,17 @@ def dist_to_root(point,root,Graph):
     path = nx.shortest_path(Graph, source = point, target = root)
     return (len(path)-1)
     
+def generate_random_graph(n,density):
+	## n - number of nodes
+	## d - "density" of the graph [0,1]
+	graph = nx.Graph()
+	for i in xrange(n):
+		for j in xrange(i,n):
+			if np.random.uniform < density:
+				graph.add_edge(i,j)
+
+	return graph 
+    
 def finding_aug_path(G,M,Blossom_stack):
     Forest = [] #Storing the Forests
     Path = [] # The final path 
